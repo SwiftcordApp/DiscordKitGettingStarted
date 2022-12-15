53cf2cfa3641d901ac4f4637efdf4a0393c76bd2
cryptoAlgorithm <cryptoalgro@gmail.com>
2022-12-15 11:49:40 +0800

    feat (commands): register hello slash command

--- DiscordKitGettingStarted/Bot.swift
+++ DiscordKitGettingStarted/Bot.swift
@@ -13,9 +13,26 @@ import DiscordKitBot // Import the DiscordKit module for all the wonders contain
 public struct Bot {
     static let bot = Client(intents: .unprivileged)
 
+    private static func registerSlashCommands() async throws {
+        try await bot.registerApplicationCommands(guild: ProcessInfo.processInfo.environment["COMMAND_GUILD_ID"]) {
+            NewAppCommand("hello", description: "Get a nice hello message") { interaction in
+                print("Received hello interaction!")
+            }
+        }
+    }
+
     public static func main() {
         bot.ready.listen {
             print("Successfully logged in as \(bot.user!.username)#\(bot.user!.discriminator)!")
+
+            // Register slash commands after logging in
+            do {
+                print("Registering interactions...")
+                try await registerSlashCommands()
+                print("Registered interactions!")
+            } catch {
+                print("Failed to register interactions: \(error.localizedDescription)")
+            }
         }
 
         // Login to Discord's API
